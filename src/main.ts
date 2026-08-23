90cba5a587547ff7c1131e42be09a1c7f9ba1357
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-08-23 12:49:32 +0530

    Scaffold basic mechanism for Python code execution

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,30 @@
+// Library
 import { loadMicroPython } from '@micropython/micropython-webassembly-pyscript'
 
+// Initialize MicroPython and setup where to display stdout and stderr
 const micropython = await loadMicroPython({
-  stdout: (text: string) => console.log("[micropython stdout]", text)
+  stdout: (text: string) => display(text),
+  stderr: (text: string) => display(text),
 })
 
-micropython.runPython(`print("Hello from MicroPython!")`)
+/** The textarea where the user can input Python code */
+const sourceCode = document.getElementById("source-code") as HTMLTextAreaElement
+/** The div where the output of the Python code will be displayed */
+const displayOutput = document.getElementById("display-output") as HTMLDivElement
+/** The button to run the Python code */
+const runButton = document.getElementById("run-button") as HTMLButtonElement
+
+// Register event listener for the run button to execute the Python code and display the output
+runButton.addEventListener("click", runCode)
+
+/** Executes the Python code entered by the user in the textarea and displays the output in the designated div */
+function runCode() {
+  const src = sourceCode.value
+  if (!src) { return }
+  micropython.runPython(src)
+}
+
+/** Displays the given text in the designated output div */
+function display(text: string) {
+  displayOutput.innerText = text
+}
