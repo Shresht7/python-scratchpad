f17bd753c5637ec3be4e84ff1eb767797087547a
Shresht Srivastav <59516096+Shresht7@users.noreply.github.com>
2026-08-25 14:17:23 +0530

    Move clear button functionality to a dedicated module

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,9 @@ import { decodeHashFragmentToCode } from "./modules/share"
 
 import './ui/icons'
 import './ui/layout'
+import './modules/clear'
 
 import "./style.css"
-
-/** The button to clear the output display */
-const clearButton = document.getElementById("clear-output") as HTMLButtonElement
-
-// Register event listener for the clear button to empty the output display
-clearButton.addEventListener("click", output.clear)
 
 /** The buttons to copy the source code and the output to the clipboard */
 const copyCodeButton = document.getElementById("copy-code") as HTMLButtonElement
